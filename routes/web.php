6513fa92e256b3a0b8d4d182c9a852e6f0c9549c
ksimpac <adasl0376210456789@gmail.com>
2021-01-06 16:32:07 +0800

    set up route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,14 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('users')->group(function () {
+    Route::get('general_info', 'generalInfoController@index')->name('general_info');
+    Route::get('education', 'educationController@index')->name('education');
+    Route::get('thesis', 'thesisController@index')->name('thesis');
+    Route::get('industry_experience', 'industryExperienceController@index')->name('industry_experience');
+    Route::get('tcase', 'tcaseController@index')->name('tcase');
+    Route::get('MOST_project', 'mostProjectController@index')->name('MOST_project');
+    Route::get('thesis_conf', 'thesisConfController@index')->name('thesis_conf');
+    Route::get('other', 'otherController@index')->name('other');
+});
