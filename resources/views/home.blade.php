daa6d003c6bb54987db910517d05b670d28c73d1
ksimpac <adasl0376210456789@gmail.com>
2021-01-09 20:21:40 +0800

    adjust grid

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-xl-2">
+        <div class="col-lg-2">
             @include('layouts.aside')
         </div>
 
-        <div class="col-xl-10">
+        <div class="col-lg-10">
             <div class="card">
                 <div class="card-header">系統訊息</div>
 
