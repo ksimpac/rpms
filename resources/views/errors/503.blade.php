8600579c3bad3c1922442e3db599637d95c367fb
ksimpac <adasl0376210456789@gmail.com>
2022-02-26 15:39:48 +0800

    add 503 page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>系統維護中</title>
+    <style>
+        *{ margin: 0px; padding: 0px; }
+        body{ font-family: Arial, sans-serif;}
+        .container{ margin: 0px 20px; padding: 20px; }
+        .text-center{ text-align: center; }
+        .title{ font-size: 30px; }
+        .subtitle{ font-size:20px; color: #aaa; margin-top: 50px; }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="text-center subtitle">系統維護中，請稍後上傳</div>
+    </div>
+</body>
+</html>
