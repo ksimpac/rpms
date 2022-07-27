5c97ad265962c32c19c324bc46eee2ecbe8edfbd
ksimpac <adasl0376210456789@gmail.com>
2022-07-27 09:55:23 +0800

    fixed route conflict

--- routes/web.php
+++ routes/web.php
@@ -49,8 +49,6 @@ Route::group(['prefix' => '/admin', 'namespace' => 'Admin', 'as' => 'admin.', 'm
 
 Route::group(['prefix' => '/users', 'middleware' => ['web']], function () {
     Route::name('general_info.')->group(function () {
-        Route::get('general_info', 'GeneralInfoController@index')->name('index');
-        Route::get('general_info/{id}', 'GeneralInfoController@show')->name('show');
         Route::middleware('isSignup', 'isDeadline')->group(function () {
             Route::get('general_info/create', 'GeneralInfoController@create')->name('create')->middleware('general_info');
             Route::post('general_info', 'GeneralInfoController@store')->name('store');
@@ -58,11 +56,11 @@ Route::group(['prefix' => '/users', 'middleware' => ['web']], function () {
             Route::patch('general_info/{id}', 'GeneralInfoController@update')->name('update');
             Route::delete('general_info/{id}', 'GeneralInfoController@destroy')->name('destroy');
         });
+        Route::get('general_info', 'GeneralInfoController@index')->name('index');
+        Route::get('general_info/{id}', 'GeneralInfoController@show')->name('show');
     });
 
     Route::name('education.')->group(function () {
-        Route::get('education', 'EducationController@index')->name('index');
-        Route::get('education/{id}', 'EducationController@show')->name('show');
         Route::middleware(['isSignup', 'isDeadline'])->group(function () {
             Route::get('education/create', 'EducationController@create')->name('create')->middleware('education');
             Route::post('education', 'EducationController@store')->name('store');
@@ -70,11 +68,11 @@ Route::group(['prefix' => '/users', 'middleware' => ['web']], function () {
             Route::patch('education/{id}', 'EducationController@update')->name('update');
             Route::delete('education/{id}', 'EducationController@destroy')->name('destroy');
         });
+        Route::get('education', 'EducationController@index')->name('index');
+        Route::get('education/{id}', 'EducationController@show')->name('show');
     });
 
     Route::name('thesis.')->group(function () {
-        Route::get('thesis', 'ThesisController@index')->name('index');
-        Route::get('thesis/{id}', 'ThesisController@show')->name('show');
         Route::middleware('isSignup', 'isDeadline')->group(function () {
             Route::get('thesis/create', 'ThesisController@create')->name('create');
             Route::post('thesis', 'ThesisController@store')->name('store');
@@ -82,11 +80,11 @@ Route::group(['prefix' => '/users', 'middleware' => ['web']], function () {
             Route::patch('thesis/{id}', 'ThesisController@update')->name('update');
             Route::delete('thesis/{id}', 'ThesisController@destroy')->name('destroy');
         });
+        Route::get('thesis', 'ThesisController@index')->name('index');
+        Route::get('thesis/{id}', 'ThesisController@show')->name('show');
     });
 
     Route::name('industry_experience.')->group(function () {
-        Route::get('industry_experience', 'IndustryExperienceController@index')->name('index');
-        Route::get('industry_experience/{id}', 'IndustryExperienceController@show')->name('show');
         Route::middleware(['isSignup', 'isDeadline'])->group(function () {
             Route::get('industry_experience/create', 'IndustryExperienceController@create')->name('create');
             Route::post('industry_experience', 'IndustryExperienceController@store')->name('store');
@@ -94,11 +92,11 @@ Route::group(['prefix' => '/users', 'middleware' => ['web']], function () {
             Route::patch('industry_experience/{id}', 'IndustryExperienceController@update')->name('update');
             Route::delete('industry_experience/{id}', 'IndustryExperienceController@destroy')->name('destroy');
         });
+        Route::get('industry_experience', 'IndustryExperienceController@index')->name('index');
+        Route::get('industry_experience/{id}', 'IndustryExperienceController@show')->name('show');
     });
 
     Route::name('tcase.')->group(function () {
-        Route::get('tcase', 'TcaseController@index')->name('index');
-        Route::get('tcase/{id}', 'TcaseController@show')->name('show');
         Route::middleware(['isSignup', 'isDeadline'])->group(function () {
             Route::get('tcase/create', 'TcaseController@create')->name('create');
             Route::post('tcase', 'TcaseController@store')->name('store');
@@ -106,11 +104,11 @@ Route::group(['prefix' => '/users', 'middleware' => ['web']], function () {
             Route::patch('tcase/{id}', 'TcaseController@update')->name('update');
             Route::delete('tcase/{id}', 'TcaseController@destroy')->name('destroy');
         });
+        Route::get('tcase', 'TcaseController@index')->name('index');
+        Route::get('tcase/{id}', 'TcaseController@show')->name('show');
     });
 
     Route::name('most_project.')->group(function () {
-        Route::get('most_project', 'MostProjectController@index')->name('index');
-        Route::get('most_project/{id}', 'MostProjectController@show')->name('show');
         Route::middleware(['isSignup', 'isDeadline'])->group(function () {
             Route::get('most_project/create', 'MostProjectController@create')->name('create');
             Route::post('most_project', 'MostProjectController@store')->name('store');
@@ -118,12 +116,12 @@ Route::group(['prefix' => '/users', 'middleware' => ['web']], function () {
             Route::patch('most_project/{id}', 'MostProjectController@update')->name('update');
             Route::delete('most_project/{id}', 'MostProjectController@destroy')->name('destroy');
         });
+        Route::get('most_project', 'MostProjectController@index')->name('index');
+        Route::get('most_project/{id}', 'MostProjectController@show')->name('show');
     });
 
 
     Route::name('thesis_conf.')->group(function () {
-        Route::get('thesis_conf', 'ThesisConfController@index')->name('index');
-        Route::get('thesis_conf/{id}', 'ThesisConfController@show')->name('show');
         Route::middleware(['isSignup', 'isDeadline'])->group(function () {
             Route::get('thesis_conf/create', 'ThesisConfController@create')->name('create');
             Route::post('thesis_conf', 'ThesisConfController@store')->name('store');
@@ -131,10 +129,11 @@ Route::group(['prefix' => '/users', 'middleware' => ['web']], function () {
             Route::patch('thesis_conf/{id}', 'ThesisConfController@update')->name('update');
             Route::delete('thesis_conf/{id}', 'ThesisConfController@destroy')->name('destroy');
         });
+        Route::get('thesis_conf', 'ThesisConfController@index')->name('index');
+        Route::get('thesis_conf/{id}', 'ThesisConfController@show')->name('show');
     });
 
     Route::name('other.')->group(function () {
-        Route::get('other', 'OtherController@index')->name('index');
         Route::middleware(['isSignup', 'isDeadline'])->group(function () {
             Route::get('other/create', 'OtherController@create')->name('create');
             Route::post('other', 'OtherController@store')->name('store');
@@ -142,6 +141,7 @@ Route::group(['prefix' => '/users', 'middleware' => ['web']], function () {
             Route::patch('other/{id}', 'OtherController@update')->name('update');
             Route::delete('other/{id}', 'OtherController@destroy')->name('destroy');
         });
+        Route::get('other', 'OtherController@index')->name('index');
     });
 
     Route::name('signup.')->group(function () {
